e266d1dc96d8b7ef048ab2f336d551a77bfabbcd
Yoriki Yamaguchi <yamaguchi@cloudpack.jp>
2016-09-07 16:58:44 +0900

    nemui

--- awsregion.go
+++ awsregion.go
@@ -1,20 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/ec2"
+	"os"
+)
 
 func main() {
-    str := `us-east-1	米国東部（バージニア北部）
-us-west-2	米国西部（オレゴン）
-us-west-1	米国西部（北カリフォルニア）
-eu-west-1	欧州（アイルランド）
-eu-central-1	欧州（フランクフルト）
-ap-southeast-1	アジアパシフィック（シンガポール）
-ap-northeast-1	アジアパシフィック（東京）
-ap-southeast-2	アジアパシフィック（シドニー）
-ap-northeast-2	アジアパシフィック (ソウル)
-ap-south-1	アジアパシフィック (ムンバイ)
-sa-east-1	南米 (サンパウロ)
-`
+	if os.Getenv("AWS_REGION") == "" {
+		os.Setenv("AWS_REGION", "us-east-1")
+	}
 
-    fmt.Printf(string(str))
+	client := ec2.New(session.New())
+
+	response, err := client.DescribeRegions(nil)
+
+	if err != nil {
+		panic(err)
+	}
+
+	for _, region := range response.Regions {
+		fmt.Println(*region.RegionName)
+	}
 }
